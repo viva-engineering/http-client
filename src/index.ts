681f244455a57b330749a901204057d67bdafd51
James Brumond <james@jbrumond.me>
2019-12-23 21:27:20 -0800

    finally get the stupid fucking types right on these abstract classes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 
 import { isNode, isBrowser } from './environment';
 import { HttpClient as BaseClient } from './http-client';
+import { HttpClientConstructor } from './shim';
 import { Logger } from '@viva-eng/logger';
+
+import { Headers, HttpClientParams, HttpRequestOptions, HttpResponse } from './http-client';
+export { Headers, HttpClientParams, HttpRequestOptions, HttpResponse } from './http-client';
 
 export let NodeHttpClient: typeof import('./node/client').NodeHttpClient;
 export let BrowserHttpClient: typeof import('./browser/client').BrowserHttpClient;
@@ -17,7 +21,7 @@ if (isBrowser) {
 	BrowserHttpClient = require('./browser/client').BrowserHttpClient;
 }
 
-export const HttpClient = isNode ? NodeHttpClient : BrowserHttpClient;
+export const HttpClient: HttpClientConstructor = (isNode ? NodeHttpClient : BrowserHttpClient) as any;
 
 /**
  * Special non-logging logger that can be used in cases where no logging behavior
---
+++ src/shim.ts
@@ -0,0 +1,19 @@
+
+import { HttpClient as BaseClient, HttpClientParams, HttpRequestOptions, HttpResponse } from './http-client';
+
+// 
+// This is some stupid shit...
+// Typescript will not allow me to properly type these two classes, despite them both being fully
+// defined (no more abstract anywhere) and both implementing identical interfaces. So... We're going
+// to create a fake "shim" class that will never actually be used aside from borrowing its type
+// after we define it.
+// 
+class HttpClient extends BaseClient {
+	protected makeRequest<T>(method: string, path: string, params: HttpRequestOptions) : Promise<HttpResponse<T>> {
+		return new Promise((resolve) => resolve());
+	}
+}
+
+export interface HttpClientConstructor {
+	new (params: HttpClientParams) : HttpClient;
+}
